1b1f3b8e8063fbc4a66acd17f206b0b999a0229d
sachin yadav <SY957307@GMAIL.COM>
2025-10-03 22:11:17 +0530

    Create emp.sql

---
+++ emp.sql
@@ -0,0 +1,54 @@
+-- Online SQL Editor to Run SQL Online.
+-- Use the editor to create new tables, insert data and all other SQL operations.
+-- create table employee(emp_id int,name  varchar,salary int,hire_date int)
+--insert into employee(emp_id,name,salary,hire_date)
+--values(101,"rohit","40000","20/10/2023");
+--insert into employee(emp_id,name,salary,hire_date)
+--values(102,"rahul","45000","21/1/2024");
+--insert into employee(emp_id,name,salary,hire_date)
+--values(103,"sumit","35000","15/3/2024");
+--insert into employee(emp_id,name,salary,hire_date)
+--values(104,"mohit","30000","22/4/2020");
+---insert into employee(emp_id,name,salary,hire_date)
+---values(105,"sumit","20000","27/7/2021");
+--insert into employee(emp_id,name,salary,hire_date)
+--values(106,"ankit","22000","23/9/2022");
+--insert into employee(emp_id,name,salary,hire_date)
+--values(110,"ayush","35000","19/1/2025");
+
+--create table dept(dept_id int,deptname varchar,emp_id int);
+--insert into dept(dept_id,deptname,emp_id)
+--values(201,"salesman",1);
+--insert into dept(dept_id,deptname,emp_id)
+--values(202,"clerk",2);
+--insert into dept(dept_id,deptname,emp_id)
+--values(203,"manager",3);
+--insert into dept(dept_id,deptname,emp_id)
+--values(205,"analys",5);
+--insert into dept(dept_id,deptname,emp_id)
+--values(206,"hr",6);
+--insert into dept(dept_id,deptname,emp_id)
+--values(207,"salesman",7);
+--insert into dept(dept_id,deptname,emp_id)
+--values(208,"president",8);
+--insert into dept(dept_id,deptname,emp_id)
+--values(209,"president",9);
+--insert into dept(dept_id,deptname,emp_id)
+--values(210,"software",10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+--select * from employee;
+--select * from dept where deptname ="clerk";
+select * from employee where salary > 25000;
+
